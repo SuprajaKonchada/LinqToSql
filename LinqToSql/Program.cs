135bf5cbd5e79b6a21681dee35c956423b2db872
SuprajaKonchada <supraja.konchada@cognine.com>
2023-11-08 19:15:11 +0530

    Add project files.

---
+++ LinqToSql/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Data.Linq;
+
+namespace LinqToSql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string connectString = "Data Source=COGNINE-L31\\SQLEXPRESS ; database = CompanyDB ; Integrated Security=true";
+
+            LinqToSqldbmlDataContext db = new LinqToSqldbmlDataContext(connectString);
+
+            //Create new Employee
+
+            Employee newEmployee = new Employee();
+            
+            newEmployee.Ename = "Supraja";
+            newEmployee.Job = "ASE";
+            newEmployee.Salary = 850000;
+            newEmployee.Dname = "Delivery";
+
+            //Add new Employee to database
+            db.Employees.InsertOnSubmit(newEmployee);
+
+            //Save changes to Database.
+            db.SubmitChanges();
+
+            //Get new Inserted Employee            
+            Employee insertedEmployee = db.Employees.FirstOrDefault(e => e.Ename.Equals("Supraja"));
+
+            Console.WriteLine("Employee ID = {0},  Name = {1}, Email = {2}, ContactNo = {3}, Address = {4}", insertedEmployee.Eno,
+                              insertedEmployee.Ename, insertedEmployee.Job,
+                             insertedEmployee.Salary, insertedEmployee.Dname);
+            Console.ReadKey();
+        }
+    }
+}
